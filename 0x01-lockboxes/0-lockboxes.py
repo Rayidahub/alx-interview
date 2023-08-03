f61c635503d88fc6d822db31f75d0c70822f7475
Raymond Gaius Yidawi <rayidatech@gmail.com>
2023-08-03 14:35:10 -0500

    Imported successfully

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,30 +1,45 @@
+
+#!/usr/bin/python3
+
+from collections import deque
+
 def canUnlockAll(boxes):
+    """
+    Check if all the boxes can be opened.
+
+    Args:
+    boxes (List[List[int]]): A list of lists where each sublist represents a box and contains keys to other boxes.
+
+    Returns:
+    bool: True if all boxes can be opened, False otherwise.
+    """
     if not boxes:
         return False
 
     num_boxes = len(boxes)
     visited = set()
-    
-    def dfs(box):
-        if box in visited:
-            return
-        visited.add(box)
+    queue = deque([0])  # Start with the first box (box 0)
+    visited.add(0)
+
+    while queue:
+        current_box = queue.popleft()
 
         # Check if all boxes are unlocked
         if len(visited) == num_boxes:
             return True
 
-        # Explore keys of the current box recursively
-        for key in boxes[box]:
-            if 0 <= key < num_boxes:
-                dfs(key)
+        # Add keys of the current box to the queue if not visited
+        for key in boxes[current_box]:
+            if key not in visited and 0 <= key < num_boxes:
+                queue.append(key)
+                visited.add(key)
 
-    dfs(0)  # Start DFS from the first box (box 0)
     return len(visited) == num_boxes
 
-# Example usage:
-boxes = [[1], [2], [3], []]
-print(canUnlockAll(boxes))  # Output: True
+if __name__ == "__main__":
+    # Example usage:
+    boxes1 = [[1], [2], [3], []]
+    print(canUnlockAll(boxes1))  # Output: True
 
-boxes = [[1, 2], [3], [4], [], []]
-print(canUnlockAll(boxes))  # Output: False
+    boxes2 = [[1, 2], [3], [4], [], []]
+    print(canUnlockAll(boxes2))  # Output: False
